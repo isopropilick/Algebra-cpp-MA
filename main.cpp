675dbdb001451ec0366cb9c4c296b9f394acbf7c
Eric Pereyra <isopropilick@icloud.com>
2024-07-10 21:39:31 -0600

    Update main.cpp
    Refactor and comment

--- main.cpp
+++ main.cpp
@@ -1,222 +1,192 @@
-#include<iostream>
-#include<iomanip>
-#include<conio.h>
-#define   SIZE   10
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <conio.h>
+
+#define SIZE 10
 using namespace std;
-int main()
-{
-    int op,ca,cb,cc,cd,ce,cf,ra,rx,r1,c1,r2,c2;
-    int a,b,c,d,e,f,i,j,k,r,n;
-    int sa[100][100],sb[100][100],suma[100][100];
-    int ma[10][10], mb[10][10], mult[10][10];
-    float ga[SIZE][SIZE],gx[SIZE],rat;
-    cout<<"Calculadora mamalona\nIngrese operacion:\n\n(1) Division sintetica\n(2) Solucion de matrices por Gauss Jordan\n(3) Suma de matices\n(4) Multiplicacion de matrices\n\n";
-    cin>>op;
-    if(op==1) {
-        cout << "Division sintetica\nIngrese numero de coeficientes\n";
-        cin >> n;
-        cout << "Ingrese coeficientes (" << n << ")\n";
-        if (n == 1) {
-            cin >> ca;
-        } else if (n == 2) {
-            cin >> ca >> cb;
-        } else if (n == 3) {
-            cin >> ca >> cb >> cc;
-        } else if (n == 4) {
-            cin >> ca >> cb >> cc >> cd;
-        } else if (n == 5) {
-            cin >> ca >> cb >> cc >> cd >> ce;
-        } else if (n == 6) {
-            cin >> ca >> cb >> cc >> cd >> ce >> cf;
-        } else {
-            cout << "Error, sintaxis invalida o numero de coeficientes mayor a 6";
+
+void synthetic_division() {
+    int n, ca, cb, cc, cd, ce, cf, ra, rx;
+    vector<int> coefficients;
+
+    cout << "Synthetic Division\nEnter number of coefficients\n";
+    cin >> n;
+
+    cout << "Enter coefficients (" << n << ")\n";
+    coefficients.resize(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> coefficients[i];
+    }
+
+    cout << "Enter root to evaluate\n";
+    cin >> ra;
+    rx = -ra;
+
+    for (int i = 1; i < n; ++i) {
+        coefficients[i] += coefficients[i - 1] * rx;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        cout << coefficients[i] << " ";
+    }
+
+    getch();
+}
+
+void gauss_jordan() {
+    int n;
+    float ga[SIZE][SIZE + 1], gx[SIZE], rat;
+
+    cout << "Matrix Solution by Gauss Jordan\nEnter number of unknowns:\n";
+    cin >> n;
+    cout << setprecision(3) << fixed;
+
+    cout << "Enter the coefficients of the matrix: " << endl;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n + 1; ++j) {
+            cout << "a[" << i << "]" << j << "]= ";
+            cin >> ga[i][j];
+        }
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        if (ga[i][i] == 0.0) {
+            cout << "Error!";
             exit(0);
         }
-        cout << "Ingrese raiz a evaluar\n";
-        cin >> ra;
-        if (n == 1) {
-            rx = (-1) * ra;
-            a = ca;
-            cout << a;
-        } else if (n == 2) {
-            rx = (-1) * ra;
-            a = ca;
-            b = cb + (a * rx);
-            cout << a << " " << b;
-        } else if (n == 3) {
-            rx = (-1) * ra;
-            a = ca;
-            b = cb + (a * rx);
-            c = cc + (b * rx);
-            cout << a << " " << b << " " << c;
-        } else if (n == 4) {
-            rx = (-1) * ra;
-            a = ca;
-            b = cb + (a * rx);
-            c = cc + (b * rx);
-            d = cd + (c * rx);
-            cout << a << " " << b << " " << c << " " << d;
-        } else if (n == 5) {
-            rx = (-1) * ra;
-            a = ca;
-            b = cb + (a * rx);
-            c = cc + (b * rx);
-            d = cd + (c * rx);
-            e = ce + (d * rx);
-            cout << a << " " << b << " " << c << " " << d << " " << e;
-        } else if (n == 6) {
-            rx = (-1) * ra;
-            a = ca;
-            b = cb + (a * rx);
-            c = cc + (b * rx);
-            d = cd + (c * rx);
-            e = ce + (d * rx);
-            f = cf + (e * rx);
-            cout << a << " " << b << " " << c << " " << d << " " << e << " " << f;
+        for (int j = 1; j <= n; ++j) {
+            if (i != j) {
+                rat = ga[j][i] / ga[i][i];
+                for (int k = 1; k <= n + 1; ++k) {
+                    ga[j][k] -= rat * ga[i][k];
+                }
+            }
         }
+    }
 
-        getch();
-        return 0;
+    for (int i = 1; i <= n; ++i) {
+        gx[i] = ga[i][n + 1] / ga[i][i];
     }
-    else if(op==2){
-        cout<<"Solucion de matrices por Gauss Jordan\nIngrese numero de incognitas:\n";
-        /* Setting precision and writing floating point values in fixed-point notation. */
-        cout<< setprecision(3)<< fixed;
-        /* Inputs */
-        /* 1. Reading number of unknowns */
-        cin>>n;
-        /* 2. Reading Augmented Matrix */
-        cout<<"Ingrese los coeficientes de la matriz: "<< endl;
-        for(i=1;i<=n;i++)
-        {
-            for(j=1;j<=n+1;j++)
-            {
-                cout<<"a["<< i<<"]"<< j<<"]= ";
-                cin>>ga[i][j];
-            }
-        }
-        /* Applying Gauss Jordan Elimination */
-        for(i=1;i<=n;i++)
-        {
-            if(ga[i][i] == 0.0)
-            {
-                cout<<"Error!";
-                exit(0);
-            }
-            for(j=1;j<=n;j++)
-            {
-                if(i!=j)
-                {
-                    rat = ga[j][i] / ga[i][i];
-                    for(k=1;k<=n+1;k++)
-                    {
-                        ga[j][k] = ga[j][k] - rat * ga[i][k];
-                    }
-                }
-            }
-        }
-        /* Obtaining Solution */
-        for(i=1;i<=n;i++)
-        {
-            gx[i] = ga[i][n+1]/ga[i][i];
-        }
-        /* Displaying Solution */
-        cout<< endl<<"Solucion: "<< endl;
-        for(i=1;i<=n;i++)
-        {
-            cout<<"x["<< i<<"] = "<< gx[i]<< endl;
-        }
-        return(0);
+
+    cout << endl << "Solution: " << endl;
+    for (int i = 1; i <= n; ++i) {
+        cout << "x[" << i << "] = " << gx[i] << endl;
     }
-    else if(op==3){
-        cout << "Enter number of rows (between 1 and 100): ";
-        cin >> r;
-        cout << "Enter number of columns (between 1 and 100): ";
-        cin >> c;
-        cout << endl << "Enter elements of 1st matrix: " << endl;
-        // Storing elements of first matrix entered by user.
-        for(i = 0; i < r; ++i)
-            for(j = 0; j < c; ++j)
-            {
-                cout << "Enter element a" << i + 1 << j + 1 << " : ";
-                cin >> sa[i][j];
-            }
-        // Storing elements of second matrix entered by user.
-        cout << endl << "Enter elements of 2nd matrix: " << endl;
-        for(i = 0; i < r; ++i)
-            for(j = 0; j < c; ++j)
-            {
-                cout << "Enter element b" << i + 1 << j + 1 << " : ";
-                cin >> sb[i][j];
-            }
-        // Adding Two matrices
-        for(i = 0; i < r; ++i)
-            for(j = 0; j < c; ++j)
-                suma[i][j] = sa[i][j] + sb[i][j];
-        // Displaying the resultant suma matrix.
-        cout << endl << "Sum of two matrix is: " << endl;
-        for(i = 0; i < r; ++i)
-            for(j = 0; j < c; ++j)
-            {
-                cout << suma[i][j] << "  ";
-                if(j == c - 1)
-                    cout << endl;
-            }
-        return 0;
+}
+
+void matrix_addition() {
+    int r, c;
+    int sa[100][100], sb[100][100], sum[100][100];
+
+    cout << "Enter number of rows (between 1 and 100): ";
+    cin >> r;
+    cout << "Enter number of columns (between 1 and 100): ";
+    cin >> c;
+
+    cout << endl << "Enter elements of 1st matrix: " << endl;
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) {
+            cout << "Enter element a" << i + 1 << j + 1 << " : ";
+            cin >> sa[i][j];
+        }
     }
-    else if(op==4){
-        cout << "Enter rows and columns for first matrix: ";
+
+    cout << endl << "Enter elements of 2nd matrix: " << endl;
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) {
+            cout << "Enter element b" << i + 1 << j + 1 << " : ";
+            cin >> sb[i][j];
+        }
+    }
+
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) {
+            sum[i][j] = sa[i][j] + sb[i][j];
+        }
+    }
+
+    cout << endl << "Sum of the two matrices is: " << endl;
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) {
+            cout << sum[i][j] << "  ";
+            if (j == c - 1) cout << endl;
+        }
+    }
+}
+
+void matrix_multiplication() {
+    int r1, c1, r2, c2;
+    int ma[10][10], mb[10][10], mult[10][10] = {0};
+
+    cout << "Enter rows and columns for the first matrix: ";
+    cin >> r1 >> c1;
+    cout << "Enter rows and columns for the second matrix: ";
+    cin >> r2 >> c2;
+
+    while (c1 != r2) {
+        cout << "Error! Column of the first matrix is not equal to row of the second.";
+        cout << "Enter rows and columns for the first matrix: ";
         cin >> r1 >> c1;
-        cout << "Enter rows and columns for second matrix: ";
+        cout << "Enter rows and columns for the second matrix: ";
         cin >> r2 >> c2;
-        // If column of first matrix in not equal to row of second matrix,
-        // ask the user to enter the size of matrix again.
-        while (c1!=r2)
-        {
-            cout << "Error! column of first matrix not equal to row of second.";
-            cout << "Enter rows and columns for first matrix: ";
-            cin >> r1 >> c1;
-            cout << "Enter rows and columns for second matrix: ";
-            cin >> r2 >> c2;
-        }
-        // Storing elements of first matrix.
-        cout << endl << "Enter elements of matrix 1:" << endl;
-        for(i = 0; i < r1; ++i)
-            for(j = 0; j < c1; ++j)
-            {
-                cout << "Enter element a" << i + 1 << j + 1 << " : ";
-                cin >> ma[i][j];
-            }
-        // Storing elements of second matrix.
-        cout << endl << "Enter elements of matrix 2:" << endl;
-        for(i = 0; i < r2; ++i)
-            for(j = 0; j < c2; ++j)
-            {
-                cout << "Enter element b" << i + 1 << j + 1 << " : ";
-                cin >> mb[i][j];
-            }
-        // Initializing elements of matrix mult to 0.
-        for(i = 0; i < r1; ++i)
-            for(j = 0; j < c2; ++j)
-            {
-                mult[i][j]=0;
-            }
-        // Multiplying matrix a and b and storing in array mult.
-        for(i = 0; i < r1; ++i)
-            for(j = 0; j < c2; ++j)
-                for(k = 0; k < c1; ++k)
-                {
-                    mult[i][j] += ma[i][k] * mb[k][j];
-                }
-        // Displaying the multiplication of two matrix.
-        cout << endl << "Output Matrix: " << endl;
-        for(i = 0; i < r1; ++i)
-            for(j = 0; j < c2; ++j)
-            {
-                cout << " " << mult[i][j];
-                if(j == c2-1)
-                    cout << endl;
-            }
-        return 0;
     }
+
+    cout << endl << "Enter elements of matrix 1:" << endl;
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c1; ++j) {
+            cout << "Enter element a" << i + 1 << j + 1 << " : ";
+            cin >> ma[i][j];
+        }
+    }
+
+    cout << endl << "Enter elements of matrix 2:" << endl;
+    for (int i = 0; i < r2; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            cout << "Enter element b" << i + 1 << j + 1 << " : ";
+            cin >> mb[i][j];
+        }
+    }
+
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            for (int k = 0; k < c1; ++k) {
+                mult[i][j] += ma[i][k] * mb[k][j];
+            }
+        }
+    }
+
+    cout << endl << "Output Matrix: " << endl;
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            cout << " " << mult[i][j];
+            if (j == c2 - 1) cout << endl;
+        }
+    }
+}
+
+int main() {
+    int op;
+    cout << "Advanced Calculator\nEnter operation:\n\n(1) Synthetic Division\n(2) Matrix Solution by Gauss Jordan\n(3) Matrix Addition\n(4) Matrix Multiplication\n\n";
+    cin >> op;
+
+    switch (op) {
+        case 1:
+            synthetic_division();
+            break;
+        case 2:
+            gauss_jordan();
+            break;
+        case 3:
+            matrix_addition();
+            break;
+        case 4:
+            matrix_multiplication();
+            break;
+        default:
+            cout << "Invalid option";
+            break;
+    }
+
+    return 0;
 }
